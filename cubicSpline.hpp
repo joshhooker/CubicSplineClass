2983dbb2a6cebc841a45c85927c4b2f369d6e5f3
Josh Hooker <joshualeehooker@gmail.com>
2017-04-15 22:45:14 -0500

    Better implementation

--- cubicSpline.hpp
+++ cubicSpline.hpp
@@ -1,7 +1,7 @@
 /*
-cubicSpline.hpp
+CubicSpline.h
 
-cubic spline interpolation. supports std::vector, std::array and standard arrays.
+Cubic spline interpolation. supports std::vector, std::array and standard arrays.
 
 -----------------------------------------------------------------------------
 MIT License
@@ -28,24 +28,25 @@ SOFTWARE.
 -----------------------------------------------------------------------------
 */
 
-#ifndef CUBIC_SPLINE_HPP
-#define CUBIC_SPLINE_HPP
+#ifndef CUBIC_SPLINE_H
+#define CUBIC_SPLINE_H
 
-#include <algorithm>
 #include <array>
 #include <assert.h>
-#include <type_traits>
 #include <vector>
 
 class CubicSpline {
 public:
-  CubicSpline();
-  void SetPoints(const std::vector<double> &x, const std::vector<double> &y);
-  CubicSpline(const std::vector<double> &x, const std::vector<double> &y);
-  template <typename T, int N, int M> CubicSpline(T (&x) [N], T (&y) [M]);
-  template <std::size_t N, std::size_t M> CubicSpline(std::array<double, N>& x, std::array<double, M>& y);
-  template <typename T> double operator()(T x) const;
-  ~CubicSpline();
+  inline CubicSpline();
+  inline ~CubicSpline();
+  inline CubicSpline(const std::vector<double> &x, const std::vector<double> &y);
+  template <typename T, int N, int M> inline CubicSpline(T (&x) [N], T (&y) [M]);
+  template <std::size_t N, std::size_t M> inline CubicSpline(std::array<double, N>& x, std::array<double, M>& y);
+  inline void SetPoints(const std::vector<double> &x, const std::vector<double> &y);
+  template <typename T, int N, int M> inline void SetPoints(T (&x) [N], T (&y) [M]);
+  template <std::size_t N, std::size_t M> inline void SetPoints(std::array<double, N>& x, std::array<double, M>& y);
+  void inline SetSpline();
+  template <typename T> inline double operator()(T x) const;
 
 private:
   size_t size;
@@ -53,105 +54,69 @@ private:
   std::vector<double> bVec, cVec, dVec;
 };
 
-CubicSpline::CubicSpline() {}
+inline CubicSpline::CubicSpline() {}
 
-void CubicSpline::SetPoints(const std::vector<double> &x, const std::vector<double> &y) {
+inline CubicSpline::~CubicSpline() {}
+
+inline CubicSpline::CubicSpline(const std::vector<double> &x, const std::vector<double> &y) {
   assert(x.size() == y.size());
   size = x.size();
   xVec = x; yVec = y;
   bVec.resize(size); cVec.resize(size); dVec.resize(size);
-  std::vector<double> h(size), alpha(size), l(size), z(size), u(size);
 
-  l[0] = 1.;
-  u[0] = 0.;
-  z[0] = 0.;
-  l[size-1] = 1.;
-  u[size-1] = 0.;
-  cVec[size-1] = 0.;
-  for(unsigned int i=0; i<size-1; i++) {
-    assert(xVec[i+1]>xVec[i]);
-    h[i] = xVec[i+1]-xVec[i];
-    if(i>0) {
-      alpha[i] = (3./h[i])*(yVec[i+1]-yVec[i]) - (3./h[i-1])*(yVec[i]-yVec[i-1]);
-      l[i] = 2.*(xVec[i+1]-xVec[i-1]) - h[i-1]*u[i-1];
-      u[i] = h[i]/l[i];
-      z[i] = (alpha[i]-h[i-1]*z[i-1])/l[i];
-    }
-  }
-  for(int i=size-2; i>-1; i--) {
-    cVec[i] = z[i]-u[i]*cVec[i+1];
-    bVec[i] = (yVec[i+1]-yVec[i])/h[i] - h[i]*(cVec[i+1]+2.*cVec[i])/3.;
-    dVec[i] = (cVec[i+1]-cVec[i])/(3.*h[i]);
-  }
+  SetSpline();
 }
 
-CubicSpline::CubicSpline(const std::vector<double> &x, const std::vector<double> &y) {
-  assert(x.size() == y.size());
-  size = x.size();
-  xVec = x; yVec = y;
-  bVec.resize(size); cVec.resize(size); dVec.resize(size);
-  std::vector<double> h(size), alpha(size), l(size), z(size), u(size);
-
-  l[0] = 1.;
-  u[0] = 0.;
-  z[0] = 0.;
-  l[size-1] = 1.;
-  u[size-1] = 0.;
-  cVec[size-1] = 0.;
-  for(unsigned int i=0; i<size-1; i++) {
-    assert(xVec[i+1]>xVec[i]);
-    h[i] = xVec[i+1]-xVec[i];
-    if(i>0) {
-      alpha[i] = (3./h[i])*(yVec[i+1]-yVec[i]) - (3./h[i-1])*(yVec[i]-yVec[i-1]);
-      l[i] = 2.*(xVec[i+1]-xVec[i-1]) - h[i-1]*u[i-1];
-      u[i] = h[i]/l[i];
-      z[i] = (alpha[i]-h[i-1]*z[i-1])/l[i];
-    }
-  }
-  for(int i=size-2; i>-1; i--) {
-    cVec[i] = z[i]-u[i]*cVec[i+1];
-    bVec[i] = (yVec[i+1]-yVec[i])/h[i] - h[i]*(cVec[i+1]+2.*cVec[i])/3.;
-    dVec[i] = (cVec[i+1]-cVec[i])/(3.*h[i]);
-  }
-}
-
-template <typename T, int N, int M> CubicSpline::CubicSpline(T (&x) [N], T (&y) [M]) {
+template <typename T, int N, int M> inline CubicSpline::CubicSpline(T (&x) [N], T (&y) [M]) {
   assert(N == M);
   size = N;
   xVec.assign(x, x+N); yVec.assign(y, y+M);
   bVec.resize(size); cVec.resize(size); dVec.resize(size);
-  std::vector<double> h(size), alpha(size), l(size), z(size), u(size);
 
-  l[0] = 1.;
-  u[0] = 0.;
-  z[0] = 0.;
-  l[size-1] = 1.;
-  u[size-1] = 0.;
-  cVec[size-1] = 0.;
-  for(int i=0; i<size-1; i++) {
-    assert(xVec[i+1]>xVec[i]);
-    h[i] = xVec[i+1]-xVec[i];
-    if(i>0) {
-      alpha[i] = (3./h[i])*(yVec[i+1]-yVec[i]) - (3./h[i-1])*(yVec[i]-yVec[i-1]);
-      l[i] = 2.*(xVec[i+1]-xVec[i-1]) - h[i-1]*u[i-1];
-      u[i] = h[i]/l[i];
-      z[i] = (alpha[i]-h[i-1]*z[i-1])/l[i];
-    }
-  }
-  for(int i=size-2; i>-1; i--) {
-    cVec[i] = z[i]-u[i]*cVec[i+1];
-    bVec[i] = (yVec[i+1]-yVec[i])/h[i] - h[i]*(cVec[i+1]+2.*cVec[i])/3.;
-    dVec[i] = (cVec[i+1]-cVec[i])/(3.*h[i]);
-  }
+  SetSpline();
 }
 
-template <std::size_t N, std::size_t M> CubicSpline::CubicSpline(std::array<double, N>& x, std::array<double, M>& y) {
+template <std::size_t N, std::size_t M> inline CubicSpline::CubicSpline(std::array<double, N>& x, std::array<double, M>& y) {
   assert(N == M);
   size = N;
   xVec.resize(size); yVec.resize(size);
   std::copy(x.begin(), x.begin()+size, xVec.begin());
   std::copy(y.begin(), y.begin()+size, yVec.begin());
   bVec.resize(size); cVec.resize(size); dVec.resize(size);
+
+  SetSpline();
+}
+
+inline void CubicSpline::SetPoints(const std::vector<double> &x, const std::vector<double> &y) {
+  assert(x.size() == y.size());
+  size = x.size();
+  xVec = x; yVec = y;
+  bVec.resize(size); cVec.resize(size); dVec.resize(size);
+
+  SetSpline();
+}
+
+template <typename T, int N, int M> inline void CubicSpline::SetPoints(T (&x) [N], T (&y) [M]) {
+  assert(N == M);
+  size = N;
+  xVec.assign(x, x+N); yVec.assign(y, y+M);
+  bVec.resize(size); cVec.resize(size); dVec.resize(size);
+
+  SetSpline();
+}
+
+template <std::size_t N, std::size_t M> inline void CubicSpline::SetPoints(std::array<double, N>& x, std::array<double, M>& y) {
+  assert(N == M);
+  size = N;
+  xVec.resize(size); yVec.resize(size);
+  std::copy(x.begin(), x.begin()+size, xVec.begin());
+  std::copy(y.begin(), y.begin()+size, yVec.begin());
+  bVec.resize(size); cVec.resize(size); dVec.resize(size);
+
+  SetSpline();
+}
+
+void inline CubicSpline::SetSpline() {
   std::vector<double> h(size), alpha(size), l(size), z(size), u(size);
 
   l[0] = 1.;
@@ -160,7 +125,7 @@ template <std::size_t N, std::size_t M> CubicSpline::CubicSpline(std::array<doub
   l[size-1] = 1.;
   u[size-1] = 0.;
   cVec[size-1] = 0.;
-  for(int i=0; i<size-1; i++) {
+  for(unsigned int i=0; i<size-1; i++) {
     assert(xVec[i+1]>xVec[i]);
     h[i] = xVec[i+1]-xVec[i];
     if(i>0) {
@@ -177,8 +142,7 @@ template <std::size_t N, std::size_t M> CubicSpline::CubicSpline(std::array<doub
   }
 }
 
-template <typename T> double CubicSpline::operator()(T x) const{
-  assert(std::is_arithmetic<T>::value);
+template <typename T> inline double CubicSpline::operator()(T x) const{
   double xs = static_cast<double>(x);
 
   int l = 0;
@@ -200,9 +164,6 @@ template <typename T> double CubicSpline::operator()(T x) const{
   else if(idx==size-1) result = yVec[size-1]+bVec[size-1]*xi+cVec[size-1]*xi*xi;
   else result = yVec[idx]+bVec[idx]*xi+cVec[idx]*xi*xi+dVec[idx]*xi*xi*xi;
   return result;
-}
-
-CubicSpline::~CubicSpline() {
 }
 
 #endif
--- cubicSpline_nonC++11.hpp
+++ cubicSpline_nonC++11.hpp
@@ -1,7 +1,7 @@
 /*
-cubicSpline.hpp
+CubicSpline.h
 
-cubic spline interpolation. supports std::vector, std::array and standard arrays.
+Cubic spline interpolation. supports std::vector, std::array and standard arrays.
 
 -----------------------------------------------------------------------------
 MIT License
@@ -28,18 +28,23 @@ SOFTWARE.
 -----------------------------------------------------------------------------
 */
 
-#ifndef CUBIC_SPLINE_HPP
-#define CUBIC_SPLINE_HPP
+#ifndef CUBIC_SPLINE_H
+#define CUBIC_SPLINE_H
 
 #include <assert.h>
+#include <type_traits>
 #include <vector>
 
 class CubicSpline {
 public:
-  CubicSpline();
-  void SetPoints(const std::vector<double> &x, const std::vector<double> &y);
-  double operator()(double xs) const;
-  ~CubicSpline();
+  inline CubicSpline();
+  inline ~CubicSpline();
+  inline CubicSpline(const std::vector<double> &x, const std::vector<double> &y);
+  template <typename T, int N, int M> inline CubicSpline(T (&x) [N], T (&y) [M]);
+  inline void SetPoints(const std::vector<double> &x, const std::vector<double> &y);
+  template <typename T, int N, int M> inline void SetPoints(T (&x) [N], T (&y) [M]);
+  void inline SetSpline();
+  template <typename T> inline double operator()(T x) const;
 
 private:
   size_t size;
@@ -47,13 +52,47 @@ private:
   std::vector<double> bVec, cVec, dVec;
 };
 
-CubicSpline::CubicSpline() {}
+inline CubicSpline::CubicSpline() {}
 
-void CubicSpline::SetPoints(const std::vector<double> &x, const std::vector<double> &y) {
+inline CubicSpline::~CubicSpline() {}
+
+inline CubicSpline::CubicSpline(const std::vector<double> &x, const std::vector<double> &y) {
   assert(x.size() == y.size());
   size = x.size();
   xVec = x; yVec = y;
   bVec.resize(size); cVec.resize(size); dVec.resize(size);
+
+  SetSpline();
+}
+
+template <typename T, int N, int M> inline CubicSpline::CubicSpline(T (&x) [N], T (&y) [M]) {
+  assert(N == M);
+  size = N;
+  xVec.assign(x, x+N); yVec.assign(y, y+M);
+  bVec.resize(size); cVec.resize(size); dVec.resize(size);
+
+  SetSpline();
+}
+
+inline void CubicSpline::SetPoints(const std::vector<double> &x, const std::vector<double> &y) {
+  assert(x.size() == y.size());
+  size = x.size();
+  xVec = x; yVec = y;
+  bVec.resize(size); cVec.resize(size); dVec.resize(size);
+
+  SetSpline();
+}
+
+template <typename T, int N, int M> inline void CubicSpline::SetPoints(T (&x) [N], T (&y) [M]) {
+  assert(N == M);
+  size = N;
+  xVec.assign(x, x+N); yVec.assign(y, y+M);
+  bVec.resize(size); cVec.resize(size); dVec.resize(size);
+
+  SetSpline();
+}
+
+void inline CubicSpline::SetSpline() {
   std::vector<double> h(size), alpha(size), l(size), z(size), u(size);
 
   l[0] = 1.;
@@ -72,14 +111,15 @@ void CubicSpline::SetPoints(const std::vector<double> &x, const std::vector<doub
       z[i] = (alpha[i]-h[i-1]*z[i-1])/l[i];
     }
   }
-  for(unsigned int i=size-2; i>-1; i--) {
+  for(int i=size-2; i>-1; i--) {
     cVec[i] = z[i]-u[i]*cVec[i+1];
     bVec[i] = (yVec[i+1]-yVec[i])/h[i] - h[i]*(cVec[i+1]+2.*cVec[i])/3.;
     dVec[i] = (cVec[i+1]-cVec[i])/(3.*h[i]);
   }
 }
 
-double CubicSpline::operator()(double xs) const {
+template <typename T> inline double CubicSpline::operator()(T x) const{
+  double xs = static_cast<double>(x);
 
   int l = 0;
   int h = size;
@@ -92,7 +132,7 @@ double CubicSpline::operator()(double xs) const {
     }
   }
 
-  int idx = l;
+  int idx = l-1;
 
   double xi = xs-xVec[idx];
   double result;
@@ -100,9 +140,6 @@ double CubicSpline::operator()(double xs) const {
   else if(idx==size-1) result = yVec[size-1]+bVec[size-1]*xi+cVec[size-1]*xi*xi;
   else result = yVec[idx]+bVec[idx]*xi+cVec[idx]*xi*xi+dVec[idx]*xi*xi*xi;
   return result;
-}
-
-CubicSpline::~CubicSpline() {
 }
 
 #endif
